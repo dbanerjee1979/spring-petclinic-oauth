0d0f76f7763d8bd9be8b80c75fcd575cdc3ff826
dbanerjee1979 <dbanerje1979+github@gmail.com>
2019-01-30 13:05:06 -0600

    Add me endpoint

---
+++ app-server/src/main/java/demo/petclinic/controllers/UserController.java
@@ -0,0 +1,26 @@
+package demo.petclinic.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+public class UserController {
+    @GetMapping(path = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> me(Principal principal) {
+        var auth = (Authentication) principal;
+        Map<String, Object> authorization = new HashMap<>();
+        authorization.put("username", auth.getName());
+        authorization.put("authorities",
+                auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(toList()));
+        return authorization;
+    }
+}
