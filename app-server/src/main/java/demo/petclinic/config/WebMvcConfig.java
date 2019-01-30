32bfaf8c089bbaef3e0bb7cf6044e3d155ad3679
dbanerjee1979 <dbanerje1979+github@gmail.com>
2019-01-30 13:05:47 -0600

    Add redirects to ui server

--- app-server/src/main/java/demo/petclinic/config/WebMvcConfig.java
+++ app-server/src/main/java/demo/petclinic/config/WebMvcConfig.java
@@ -1,15 +1,19 @@
 package demo.petclinic.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${demo.ui.server.url}")
+    private String uiServer;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(uiServer)
                 .allowedMethods("*");
     }
 }
--- app-server/src/main/java/demo/petclinic/config/WebSecurityConfig.java
+++ app-server/src/main/java/demo/petclinic/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package demo.petclinic.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,11 +17,27 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${demo.ui.server.url}")
+    private String uiServer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login**").permitAll()
+                .anyRequest().authenticated()
+            .and().formLogin()
+                .successHandler(new SimpleUrlAuthenticationSuccessHandler(uiServer))
+                .permitAll()
+            .and().logout()
+                .logoutSuccessUrl(uiServer)
+            .and().csrf().disable();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedOrigins(Collections.singletonList(uiServer));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("Content-Type", "Cache-Control", "Authorization"));
