47a679361975f08f366701672152a1966331c2f0
unknown <dbanerje1979+github@gmail.com>
2019-02-02 07:03:24 -0600

    Add JWT support

--- auth-server/src/main/java/demo/petclinic/auth/config/AuthServerConfig.java
+++ auth-server/src/main/java/demo/petclinic/auth/config/AuthServerConfig.java
@@ -1,12 +1,19 @@
 package demo.petclinic.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.approval.InMemoryApprovalStore;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AuthServerConfig implements AuthorizationServerConfigurer {
@@ -34,6 +41,29 @@ public class AuthServerConfig implements AuthorizationServerConfigurer {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.approvalStore(new InMemoryApprovalStore());
+        endpoints.approvalStore(new InMemoryApprovalStore())
+                 .accessTokenConverter(tokenConverter())
+                 .tokenStore(tokenStore())
+                 .tokenServices(tokenServices());
+    }
+
+    @Bean
+    public DefaultTokenServices tokenServices() {
+        var tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenEnhancer(tokenConverter());
+        tokenServices.setTokenStore(tokenStore());
+        return tokenServices;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(tokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter tokenConverter() {
+        var tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("secret-key");
+        return tokenConverter;
     }
 }
